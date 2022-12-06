811e64a5091263d789455bd124a11b8d94cceec6
averichev <averichev@yandex.ru>
2022-12-06 16:34:05 +0300

    improvement: Dependency injection proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,42 @@
-use veruna_kernel::sites::{Site, SiteId, SiteReadOption, SiteRepository as SiteRepositoryContract};
+use std::collections::HashMap;
+use veruna_kernel::sites::{Site, SiteId, SiteIdBuilderImpl, SiteReadOption, SiteRepository as SiteRepositoryContract};
 use veruna_kernel::sites::site_kit::SiteKit;
 use shaku::{Component, Provider};
+use std::ops::Deref;
 
 #[derive(Component)]
 #[shaku(interface = veruna_kernel::sites::SiteRepository)]
 pub struct SiteRepositoryImpl {
-    sites: Vec<Box<dyn Site>>,
+    sites: HashMap<u8, Box<dyn Site>>,
 }
 
 impl SiteRepositoryContract for SiteRepositoryImpl {
     fn create(&mut self, site: Box<dyn Site>) -> Box<dyn SiteId> {
-        self.sites.push(site);
+        self.sites.insert(42, site);
         let builder = SiteKit::site_id_builder();
         builder.build(42)
     }
 
-    fn read(&self, read_by: SiteReadOption) -> (Box<dyn Site>, Box<dyn SiteId>) {
-        todo!()
+    fn read(&self, read_by: SiteReadOption) -> (&Box<dyn Site>, Box<dyn SiteId>) {
+        match read_by {
+            SiteReadOption::SiteId(id) => {
+                let id = id.value();
+                let site = self.sites.get(&id).unwrap();
+                let builder = SiteKit::site_id_builder();
+                let site_id = builder.build(id);
+                let result = (site, site_id);
+
+                result
+            }
+            SiteReadOption::Domain(_) => {
+                let id = 42;
+                let site = self.sites.get(&id).unwrap();
+                let builder = SiteKit::site_id_builder();
+                let site_id = builder.build(id);
+                let result = (site, site_id);
+                result
+            }
+        }
     }
 
     fn delete(&self, site_id: Box<dyn Site>) -> bool {
