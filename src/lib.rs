6a3e43de5396681454f1c48d56eb2be8ee559d2f
averichev <averichev@yandex.ru>
2022-12-06 14:50:09 +0300

    improvement: Dependency injection proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 use veruna_kernel::sites::{Site, SiteId, SiteReadOption, SiteRepository as SiteRepositoryContract};
-use shaku::{Provider};
+use veruna_kernel::sites::site_kit::SiteKit;
+use shaku::{Component, Provider};
 
-#[derive(Provider)]
-#[shaku(interface = SiteRepositoryContract)]
-pub struct SiteRepository {}
+#[derive(Component)]
+#[shaku(interface = veruna_kernel::sites::SiteRepository)]
+pub struct SiteRepositoryImpl {
+    sites: Vec<Box<dyn Site>>,
+}
 
-impl SiteRepositoryContract for SiteRepository {
-    fn create(&self, site: Box<dyn Site>) -> Box<dyn SiteId> {
-        todo!()
+impl SiteRepositoryContract for SiteRepositoryImpl {
+    fn create(&mut self, site: Box<dyn Site>) -> Box<dyn SiteId> {
+        self.sites.push(site);
+        let builder = SiteKit::site_id_builder();
+        builder.build(42)
     }
 
     fn read(&self, read_by: SiteReadOption) -> (Box<dyn Site>, Box<dyn SiteId>) {
